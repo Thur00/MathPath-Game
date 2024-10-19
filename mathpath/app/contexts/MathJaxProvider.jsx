72f601c8bfbc9e775771e1dc876ea51343ef1a60
Thur00 <arthursvinhedo@gmail.com>
2024-10-18 21:07:42 -0300

    Correção do erro de window

--- mathpath/app/contexts/MathJaxProvider.jsx
+++ mathpath/app/contexts/MathJaxProvider.jsx
@@ -10,21 +10,25 @@ export function MathJaxProvider({ children }) {
   const [mathJax, setMathJax] = useState(null);
 
   useEffect(() => {
-    if (!window.MathJax) {
-      const script = document.createElement("script");
-      script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js";
-      script.async = true;
-      script.onload = () => {
-        window.MathJax = MathJax;
-        MathJax.config = mathJaxConfig;
-        MathJax.startup.promise = MathJax.startup.promise
-          .then(() => MathJax.typesetPromise())
-          .catch((err) => console.error("Erro ao processar MathJax:", err));
-        setMathJax(MathJax);
-      };
-      document.head.appendChild(script);
-    } else {
-      setMathJax(window.MathJax);
+    // Garantir que a configuração seja usada apenas no client-side
+    if (typeof window !== "undefined") {
+      if (!window.MathJax) {
+        const script = document.createElement("script");
+        script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js";
+        script.async = true;
+        script.onload = () => {
+          window.MathJax = MathJax;
+          // MathJax só é configurado quando o script é carregado no client-side
+          MathJax.config = mathJaxConfig;
+          MathJax.startup.promise = MathJax.startup.promise
+            .then(() => MathJax.typesetPromise())
+            .catch((err) => console.error("Erro ao processar MathJax:", err));
+          setMathJax(MathJax);
+        };
+        document.head.appendChild(script);
+      } else {
+        setMathJax(window.MathJax);
+      }
     }
   }, []);
 
