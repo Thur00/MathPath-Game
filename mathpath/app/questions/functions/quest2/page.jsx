cc40097866b990ddbedfa7c1f562482fc96b9940
Ana Lobrigato <129215023+analobrigato@users.noreply.github.com>
2024-11-22 08:40:57 -0300

    Update QUEST3.jsx

--- mathpath/app/questions/functions/quest2/page.jsx
+++ mathpath/app/questions/functions/quest2/page.jsx
@@ -9,20 +9,81 @@ export default function Quest() {
   const contas = [
     {
       id: 1,
-      title: "Para encontrar o valor de x em",
-      content: "\\(f(x) = 2x + 3 = 11\\)",
+      title: "qual o valor de x para f(x)=4",
+      content: "\\(f(x)= x^2-2x+1\\)",
     },
     {
       id: 2,
-      title: "Devemos tirar três dos dois lados:",
-      content: "\\(2x = 11 - 3\\)",
+      title: "Iguale a equação a 4",
+      content: "\\(f(x)= x^2-2x+1=4\\)",
     },
     {
       id: 3,
-      title: "E então isolar o x:",
-      content: "\\(x = \\frac{8}{2}\\)",
+      title: "Reorganize os termos",
+      content: "\\(f(x)= x^2-2x+1-4=0\\)",
     },
-    { id: 4, title: "Calculando:", content: "\\(x = 4\\)" },
+    { id: 4, content: "\\(f(x)= x^2-2x-3=0\\)" },
+
+    { id: 5, 
+     title: "Aplique a fórmula de Bhaskara",
+      content: "\\(x = \\frac{-b \\pm \\sqrt{b^2 - 4 \\cdot a \\cdot c}}{2 \\cdot a}\\)"},
+
+      {
+      id: 6,
+      title: "E considerando os valores:",
+      content: "\\(a=1 \\text{, } b=-2 \\text{ e } c=-3\\)"},
+
+    { 
+      id: 7,
+      title: "Substitua os valores e resolva:",
+      content:
+        "\\(x = \\frac{-(-2) \\pm \\sqrt{(-2)^2 + 12 \\cdot a \\cdot c}}{2 \\cdot a}\\)",
+    },
+
+    {     id: 8,
+      content: "\\(x = \\frac{2 \\pm \\sqrt{4 + 12}}{2}\\)",
+    },
+
+     {
+      id: 9,
+      content: "\\(x = \\frac{2 \\pm \\sqrt{16}}{2}\\)",
+    }, 
+
+    {
+      id: 10,
+      content: "\\(x = \\frac{2 \\pm 4}{2}\\)",
+    },
+
+     {
+      id: 11,
+      title: "E então:",
+      content: "\\(x_1 = \\frac{2 + 4}{2}\\)",
+    },
+       {
+      id: 12,
+      content: "\\(x_1 = \\frac{6}{2} = 3\\)",
+    },
+
+     {
+      id: 13,
+      content: "\\(x_2 = \\frac{2 - 4}{2}\\)",
+    },
+        {
+      id: 14,
+      content: "\\(x_2 = \\frac{-2}{2} = -1  \\)",
+    },
+
+
+
+
+
+
+
+
+    
+
+
+     
   ];
 
   let title;
